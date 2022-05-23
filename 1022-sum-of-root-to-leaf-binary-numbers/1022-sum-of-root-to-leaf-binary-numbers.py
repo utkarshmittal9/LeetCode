62417b7e2018d3df3135a11fe000e73bf8b136bb
Utkarsh Mittal <70594614+utkarshmittal9@users.noreply.github.com>
2022-05-23 21:32:09 +0530

    Time: 67 ms (20.01%), Space: 14.1 MB (83.32%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,28 +6,13 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        self.sum = 0
-        def get_int_fromBinary(binary):
-            binary1 = binary
-            decimal, i, n = 0, 0, 0
-            while(binary != 0):
-                dec = binary % 10
-                decimal = decimal + dec * pow(2, i)
-                binary = binary//10
-                i += 1
-            return decimal   
-                
-        def inorder(root, prev):
-            print("prev initial:",prev)
-            if not root:return
+        def _sum(root,res):
+            if not root:
+                return 0
+            res = res*2 + root.val
             if not root.left and not root.right:
-                prev = prev + str(root.val)
-                self.sum = self.sum + get_int_fromBinary(int(prev))
-                return
-            inorder(root.left, prev+str(root.val))
-            print("from middle", prev, root.val)
-            inorder(root.right, prev+str(root.val))
-        inorder(root,"")
-        return self.sum
+                return res
+            return _sum(root.left,res) + _sum(root.right,res)
+        return _sum(root,0)
                 
         

164390443af6ff6d23c296058d6a1ce43b6b73a1
Utkarsh Mittal <70594614+utkarshmittal9@users.noreply.github.com>
2022-05-24 15:27:38 +0530

    Time: 32 ms (90.80%), Space: 13.9 MB (21.74%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -4,11 +4,9 @@ class Solution:
         word1 = Counter(word1)
         word2 = Counter(word2)
         for word,value in word1.items():
-            print(word,value,word2.get(word,0))
             if abs(value-word2.get(word,0))>3:
                 return False
         for word,value in word2.items():
-            print(word,value,word1.get(word,0))
             if abs(value-word1.get(word,0))>3:
                 return False
         return True

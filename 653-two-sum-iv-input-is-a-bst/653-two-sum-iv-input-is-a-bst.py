099e9ae082d42ef1b8049ae3a87aa248fb366f7d
Utkarsh Mittal <70594614+utkarshmittal9@users.noreply.github.com>
2022-05-29 15:18:32 +0530

    Time: 128 ms (39.19%), Space: 20.2 MB (13.54%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        self.list= {}
+        def inorder(root):
+            if root:
+                inorder(root.left)
+                self.list[root.val] = True
+                inorder(root.right)
+        inorder(root)
+        print(self.list)
+        for i in self.list.keys():
+            if self.list.get(k-i) and k-i!=i:
+                return True
+        return False

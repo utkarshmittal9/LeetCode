6ad59649b3baf3925ca7f76ad171a6294e58514a
Utkarsh Mittal <70594614+utkarshmittal9@users.noreply.github.com>
2022-09-13 22:57:54 +0530

    Time: 284 ms (5.07%), Space: 14.9 MB (14.69%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def check_cycle(n, adj_list, visited, parent):
+            visited.add(n)
+            for i in adj_list[n]:
+                if i not in visited:
+                    if check_cycle(i, adj_list, visited, n):
+                        return True
+                elif i!=parent:
+                    return True
+            return False
+        def check_tree(edges_trial):
+            print(edges_trial)
+            adj_list = defaultdict(list)
+            for i,j in edges_trial:
+                adj_list[i].append(j)
+                adj_list[j].append(i)
+            print(adj_list)
+            visited = set()
+            for i in range(1,len(adj_list)+1):
+                if i not in visited:
+                    result = check_cycle(i, adj_list, visited, -1)
+                    if result:
+                        return True
+            return False
+        for i in range(len(edges)-1,-1,-1):
+            edges_trial = edges[:i]+edges[i+1:]
+            if not check_tree(edges_trial):
+                return edges[i]

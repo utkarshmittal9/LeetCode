93d1aa3c2ab2203538508155bdb919b2dd2a452d
Utkarsh Mittal <70594614+utkarshmittal9@users.noreply.github.com>
2022-09-18 18:52:30 +0530

    Time: 493 ms (52.92%), Space: 16.6 MB (47.95%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,24 @@
 class Solution:
-    def numIslands(self, grid):
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(row, col, visited):
+            if row < 0 or col < 0 or row >= max_row or col >= max_col or grid[row][col]!="1":
+                return 
+            grid[row][col] = "#"
+            dfs(row+1, col,visited)
+            dfs(row-1, col,visited)
+            dfs(row, col+1,visited)
+            dfs(row, col-1,visited)
+            
         if not grid:
             return 0
-
-        count = 0
+        island = 0
+        visited = set()
+        max_row = len(grid)
+        max_col = len(grid[0])
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
-                    count += 1
-        return count
-
-    def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '#'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
+                if grid[i][j]=="1":
+                    dfs(i,j, visited)
+                    island+=1
+        return island
         
